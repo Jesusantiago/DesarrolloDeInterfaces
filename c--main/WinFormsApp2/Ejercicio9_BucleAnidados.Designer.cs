b4c324bac3d1e7a5e995f488fe66a850f1f9621a
Jesus Santiago <jesusantiago420@gmail.com>
2025-03-11 10:28:16 +0100

    Bucle anidado - Entregado

---
+++ c--main/WinFormsApp2/Ejercicio9_BucleAnidados.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace WinFormsApp2;
+
+partial class Ejercicio9_BucleAnidados
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        listBox = new System.Windows.Forms.ListBox();
+        SuspendLayout();
+        // 
+        // listBox
+        // 
+        listBox.FormattingEnabled = true;
+        listBox.Location = new System.Drawing.Point(31, 26);
+        listBox.Name = "listBox";
+        listBox.Size = new System.Drawing.Size(586, 379);
+        listBox.TabIndex = 0;
+
+        // 
+        // Ejercicio9_BucleAnidados
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(660, 440);
+        Controls.Add(listBox);
+        Text = "Ejercicio9_BucleAnidados";
+        Load += Ejercicio9_BucleAnidados_Load;
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.ListBox listBox;
+
+    #endregion
+}
--- c--main/WinFormsApp2/obj/Debug/net9.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ c--main/WinFormsApp2/obj/Debug/net9.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd26409a67513ca7020735b78410e222e90a0bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb2c01509e71635eec995732f85c72aa08ef18c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
