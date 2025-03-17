57747a43dd791cfd8f2028f6e62f3a508bcc8668
Jesus Santiago <jesusantiago420@gmail.com>
2025-03-17 09:27:38 +0100

    ejercicio 9 Bucle Anidados - Entregado

--- c--main/WinFormsApp2/obj/Debug/net9.0-windows/WinFormsApp2.AssemblyInfo.cs
+++ c--main/WinFormsApp2/obj/Debug/net9.0-windows/WinFormsApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c324bac3d1e7a5e995f488fe66a850f1f9621a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3a79d5181e040db1ce666a617e1aa832c3d934")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
