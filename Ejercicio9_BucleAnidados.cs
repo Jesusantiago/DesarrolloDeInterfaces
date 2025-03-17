a3e1c921c91ef59d4c348b6fc946a731a0e9cdb5
Jesus Santiago <jesusantiago420@gmail.com>
2025-03-17 09:31:28 +0100

    Ejercicio 9 - Bucle anidado - Entregado

---
+++ Ejercicio9_BucleAnidados.cs
@@ -0,0 +1,27 @@
+﻿namespace WinFormsApp2;
+
+public partial class Ejercicio9_BucleAnidados : Form
+{
+    public Ejercicio9_BucleAnidados()
+    {
+        InitializeComponent();
+        
+        
+    }
+    
+    private void Ejercicio9_BucleAnidados_Load(object sender, EventArgs e)
+    {
+        
+        for (int i = 0; i <= 10; i++)
+        {
+            for (int j = 0; j <= 10; j++)
+            {
+                listBox.Items.Add(i + " x " + j + " = " + (i * j));
+            }
+
+            listBox.Items.Add("\n");
+        }
+        
+        // throw new System.NotImplementedException();
+    }
+}
